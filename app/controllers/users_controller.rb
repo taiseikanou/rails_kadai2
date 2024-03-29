5709d391576e2153d74c03e5abd78f9614d9bc7f
加納大成 <kanoutaisei@kanoutaiseinoMacBook-Air.local>
2024-03-29 20:55:41 +0900

    accountの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  def edit
+  end
+  def show
+  end
+  def update
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def destroy
+  end
+
   def user_params
-    params.require(:user).permit(:name,:introduction,:email,:password)
+    params.require(:user).permit(:name,:introduction,:email)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'reservations/confirm'
   resources :reservations
   resources :rooms
-  resources :users
+  resources :users, only: [:edit]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
