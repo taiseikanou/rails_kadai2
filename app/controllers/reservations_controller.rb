519e1d87c315181a46cf555c3446796728411e69
加納大成 <kanoutaisei@kanoutaiseinoMacBook-Air.local>
2024-04-01 14:38:19 +0900

    ビューの変更と予約の編集

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,12 +17,22 @@ class ReservationsController < ApplicationController
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
   end
 
   def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
+      redirect_to root_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to root_path
   end
 
   private
